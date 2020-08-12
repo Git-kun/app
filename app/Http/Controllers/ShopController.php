4050d96c549ff5bee0c2bcfab0b29618acd64cef
Git-kun <56929408+Git-kun@users.noreply.github.com>
2020-08-13 00:14:58 +0900

    shopコントローラー追記変更

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -11,6 +11,6 @@ class ShopController extends Controller
     public function index() //追加
     {
         $stocks = Stock::Paginate(6); //Eloquantで検索
-        return view('shop');
+        return view('shop',compact('stocks')); //追記変更
     }
 }
