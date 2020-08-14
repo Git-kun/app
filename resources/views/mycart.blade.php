b26657da37a5c0be87f75069d05537bdb68f2cb3
Git-kun <56929408+Git-kun@users.noreply.github.com>
2020-08-14 22:10:01 +0900

    ビュー変更

--- resources/views/mycart.blade.php
+++ resources/views/mycart.blade.php
@@ -4,21 +4,21 @@
 <div class="container-fluid">
    <div class="">
        <div class="mx-auto" style="max-width:1200px">
-           <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">商品一覧</h1>
-           <div class="">
-               <div class="d-flex flex-row flex-wrap">
-                 商品一覧を出したい
+          <h1 class="text-center font-weight-bold" style="color:#555555;
+                font-size:1.2em; padding:24px 0px;">カートの中身</h1>
+            <div class="card-body">
+                  <div class="d-flex flex-row flex-wrap">
+                  商品一覧を出したい
 
-                {{-- 追加 --}}
+                  {{-- 追加 --}}
 
-                @foreach($my_carts as $my_cart)
-                      {{$my_cart->user_id}} <br>
-                      {{$my_cart->stock_id}}<br>
-                 @endforeach
-
-                {{-- ここまで --}}
-               </div>
-           </div>
+                  @foreach($my_carts as $my_cart)
+                        {{$my_cart->user_id}} <br>
+                        {{$my_cart->stock_id}}<br>
+                  @endforeach
+                  {{-- ここまで --}}
+                  </div>
+            </div>
        </div>
    </div>
 </div>
