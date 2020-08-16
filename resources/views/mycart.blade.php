f3557feab37362ffd2b5462e0dc3dc6c6ca7b4c1
Git-kun <56929408+Git-kun@users.noreply.github.com>
2020-08-17 00:19:05 +0900

    <div>ダグ修正

--- resources/views/mycart.blade.php
+++ resources/views/mycart.blade.php
@@ -6,16 +6,12 @@
        <div class="mx-auto" style="max-width:1200px">
           <h1 class="text-center font-weight-bold" style="color:#555555; font-size:1.2em; padding:24px 0px;">カートの中身</h1>
             <div class="card-body">
-                  {{-- 追加 --}}
-
                   @foreach($my_carts as $my_cart)
-                        {{$my_cart->user_id}} <br>
+                        {{$my_cart->user_id}}<br>
                         {{$my_cart->stock_id}}<br>
                   @endforeach
-                  {{-- ここまで --}}
-                  </div>
             </div>
-       </div>
+        </div>
    </div>
 </div>
 @endsection
