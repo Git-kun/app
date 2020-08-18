1292649ffb68506402e6997f0201cfb41c4dfd69
Git-kun <56929408+Git-kun@users.noreply.github.com>
2020-08-19 02:27:08 +0900

    mycartビュー　レイアウト調整

--- resources/views/mycart.blade.php
+++ resources/views/mycart.blade.php
@@ -9,17 +9,14 @@
 
            <div class="">
                <p class="text-center">{{ $message }}</p><br>
-               <div class="d-flex flex-row flex-wrap">
-
+               <!-- <div class="d-flex flex-row flex-wrap"> -->
                    @foreach($my_carts as $my_cart)
                        <div class="mycart_box">
                            <p>ユーザーID：{{$my_cart->user_id}}</p>
                            <p>ストックID：{{$my_cart->stock_id}}</p>
                        </div>
                    @endforeach
-
                </div>
-
                <a href="/">商品一覧へ</a>
            </div>
        </div>
