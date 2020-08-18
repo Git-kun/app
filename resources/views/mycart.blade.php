49aceb866a6f5ae40c4f4bfb4fea3dab946772c9
Git-kun <56929408+Git-kun@users.noreply.github.com>
2020-08-19 02:23:03 +0900

    レイアウト調整

--- resources/views/mycart.blade.php
+++ resources/views/mycart.blade.php
@@ -4,9 +4,11 @@
 <div class="container-fluid">
    <div class="">
        <div class="mx-auto" style="max-width:1200px">
-          <h1 class="text-center font-weight-bold" style="color:#555555; font-size:1.2em; padding:24px 0px;">{{ Auth::user()->name }}さんのカートの中身</h1>
-            <div class="">
-               <p class="text-center">{{ $message ?? '' }}</p><br>
+           <h1 class="text-center font-weight-bold" style="color:#555555;  font-size:1.2em; padding:24px 0px;">
+           {{ Auth::user()->name }}さんのカートの中身</h1>
+
+           <div class="">
+               <p class="text-center">{{ $message }}</p><br>
                <div class="d-flex flex-row flex-wrap">
 
                    @foreach($my_carts as $my_cart)
@@ -15,9 +17,12 @@
                            <p>ストックID：{{$my_cart->stock_id}}</p>
                        </div>
                    @endforeach
-            </div>
-            <a href="/">商品一覧へ</a>
-        </div>
+
+               </div>
+
+               <a href="/">商品一覧へ</a>
+           </div>
+       </div>
    </div>
 </div>
 @endsection
