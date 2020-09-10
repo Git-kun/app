f78c8cf4a70ed0e156c58490ed57fe7418dd6903
Git-kun <56929408+Git-kun@users.noreply.github.com>
2020-09-11 00:43:24 +0900

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,7 +101,6 @@
         <br>
         <p style="font-size:2.4em">Shopsite</p><br>
         </div>
-        <!-- <p style="font-size:0.7em;">@copyright @mukae9</p> -->
        </footer>
    </div>
 </body>
