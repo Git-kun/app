41921490ef1e0ec96362201a0d6632986bd291ac
Git-kun <56929408+Git-kun@users.noreply.github.com>
2020-09-19 23:09:32 +0900

    Update ShopController.php

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -18,7 +18,7 @@ class ShopController extends Controller
         return view('shop',compact('stocks')); 
     }
 
-    public function myCart(Cart $cart)
+    public function myCart(Cart $cart) //カートを確認
     {
         $data = $cart->showCart();
         return view('mycart',$data); 
