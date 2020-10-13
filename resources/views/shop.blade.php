b6a3644bfbfbb58cd31ea7ad01f850f6951120a2
Git-kun <56929408+Git-kun@users.noreply.github.com>
2020-10-13 23:50:47 +0900

    Update shop.blade.php

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -8,7 +8,6 @@
            <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">商品一覧</h1>
            <div class="">
                <div class="d-flex flex-row flex-wrap">
-           
                        @foreach($stocks as $stock) <!--商品情報を$stocksでforeach文で回す -->
                            <div class="col-xs-6 col-sm-4 col-md-4 ">
                                <div class="max-w-sm rounded overflow-hidden shadow-lg">
