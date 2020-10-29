15ec728b173d415fa9161a0e67ee49492a17a4a1
Git-kun <56929408+Git-kun@users.noreply.github.com>
2020-10-29 21:54:54 +0900

    Update shop.blade.php

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -4,7 +4,6 @@
 <div class="container-fluid">
    <div class="">
        <div class="mx-auto" style="max-width:1200px">
-        
            <!-- <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">商品一覧</h1> -->
            <h1 class="text-current text-center text-xl py-6 font-semibold">商品一覧</h1>
            <div class="">
